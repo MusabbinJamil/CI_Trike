e21360aa8399d510fe134a7936780d767f29cf8e
adnan495r <adnan495r@gmail.com>
2025-04-27 19:58:48 +0500

    Add files via upload

--- trike2/trike_ai/agents/__init__.py
+++ trike2/trike_ai/agents/__init__.py
@@ -2,6 +2,7 @@ from trike_ai.agents.ai_base import AIBase
 from trike_ai.agents.random_ai import RandomAI
 from trike_ai.agents.minimax_ai import MinimaxAI
 from trike_ai.agents.mcts_ai import MCTSAI
+from trike_ai.agents.hybrid_ai import HybridAI
 
 # Export all agents
-__all__ = ['AIBase', 'RandomAI', 'MinimaxAI', 'MCTSAI']
+__all__ = ['AIBase', 'RandomAI', 'MinimaxAI', 'MCTSAI', 'HybridAI']
